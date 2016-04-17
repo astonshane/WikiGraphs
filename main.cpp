51751cefa977cfcc6de045b1783a2ffb3f79a4ff
Shane Aston <astonshane@gmail.com>
2016-04-16 21:43:27 -0400

    pull out links and store them in a map

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <algorithm>
 
 int g_world_size;
 int g_my_rank;
@@ -11,6 +12,7 @@ std::string g_filename = "enwiki-20160305-pages-articles.xml";
 
 void parse_file(MPI_File *infile);
 void regex_test();
+void find_links(std::string section, std::string current_title, std::map<std::string, std::vector<std::string>> &links);
 
 
 int main(int argc, char** argv) {
@@ -28,10 +30,10 @@ int main(int argc, char** argv) {
 
   MPI_File infile;
   MPI_File_open(MPI_COMM_WORLD, g_filename.c_str(), MPI_MODE_RDONLY, MPI_INFO_NULL, &infile);
-  //parse_file(&infile);
+  parse_file(&infile);
 
   if (g_my_rank == 0){
-    regex_test();
+    //regex_test();
   }
 
   MPI_File_close(&infile);
@@ -50,7 +52,7 @@ void parse_file(MPI_File *infile){
   long long int max_size = filesize / g_world_size;
 
   //get the start point for this rank in the file
-  MPI_Offset offset = max_size * g_my_rank;
+  MPI_Offset offset = std::max(max_size * g_my_rank, max_size * g_my_rank - 1000);
 
   printf("Rank %d ==> filesize: %lld ==> chunk size: %lld ==> start point: %lld\n", g_my_rank, filesize, max_size, offset);
 
@@ -62,12 +64,13 @@ void parse_file(MPI_File *infile){
 
   std::map<std::string, std::vector<std::string>> links;
 
-  while (offset < max_size && links.size() < 100){
+  while (links.size() < 500){
     chunk = (char *)malloc( (chunk_size + 1)*sizeof(char));
     MPI_File_read_at(*infile, offset, chunk, chunk_size, MPI_CHAR, MPI_STATUS_IGNORE);
     chunk[chunk_size] = '\0';
     //printf("read in: %s", chunk);
     std::string chunk_string(chunk);
+    free(chunk);
 
     std::smatch title_match;
     if (std::regex_search(chunk_string, title_match, title_regex)){
@@ -76,37 +79,56 @@ void parse_file(MPI_File *infile){
           std::string piece = sub_match.str();
           std::cout << "  submatch " << i << ": " << piece << '\n';
       }*/
+      std::string prefix = title_match.prefix();
+      find_links(prefix, current_title, links);
+
       current_title = title_match[1].str();
       links[current_title] = std::vector<std::string>();
+
+      std::string suffix = title_match.suffix();
       //std::cout << "new page:" << current_title << std::endl;
+      find_links(suffix, current_title, links);
+    }else{
+      find_links(chunk_string, current_title, links);
     }
 
     offset += chunk_size;
-    free(chunk);
   }
+
   for (const auto &key : links) {
-    std::cout << key.first << std::endl;
-    for (const auto &val: key.second){
-      std::cout << "  " << val << std::endl;
-    }
+    std::cout << g_my_rank << "  "<< key.first << std::endl;
   }
 
 }
 
+void find_links(std::string section, std::string current_title, std::map<std::string, std::vector<std::string>> &links){
+  //links[current_title].push_back("test");
+  //std::cout << section << std::endl;
+  std::regex link_regex("\\[\\[([^ ]*)\\]\\]");
+  std::smatch link_match;
+  while (std::regex_search(section, link_match, link_regex)){
+    std::ssub_match sub_match = link_match[1];
+    std::string piece = sub_match.str();
+    //std::cout << piece << '\n';
+    links[current_title].push_back(piece);
+    //std::cout << current_title << " > " << piece << std::endl;
+    section = link_match.suffix().str();
+  }
+}
+
 void regex_test(){
-  std::regex title_regex("<title>(.+)</title>");
-  std::smatch title_match;
-  std::string chunk_string = "this came before <title>test title</title> this is after";
-  if (std::regex_search(chunk_string, title_match, title_regex)){
-    for (size_t i = 0; i < title_match.size(); ++i) {
-        std::ssub_match sub_match = title_match[i];
-        std::string piece = sub_match.str();
-        std::cout << "  submatch " << i << ": " << piece << '\n';
-        std::cout << "    prefix " << title_match.prefix() << std::endl;
-        std::cout << "    suffix " << title_match.suffix() << std::endl;
-    }
-  }else{
-    std::cout << "no match" << std::endl;
+  //std::regex link_regex("\\[\\[(.+)\\]\\]");
+
+  std::string s ("this [[subject]] has a submarine as [[a]] subsequence");
+  std::smatch m;
+  std::regex e ("\\[\\[([^ ]*)\\]\\]");   // matches words beginning by "sub"
+
+  std::cout << "The following matches and submatches were found:" << std::endl;
+
+  while (std::regex_search (s,m,e)) {
+    for (auto x:m) std::cout << x << "\n";
+    std::cout << std::endl;
+    s = m.suffix().str();
   }
 
 }
