6128b2229e788907e3f3405dce49ba4273b85f7c
vbhagwani <bhagwani.vatsal@gmail.com>
2016-05-02 23:05:02 -0400

    use 125 files instead of large file, still having memory issues with certain configs of ranks

--- main.cpp
+++ main.cpp
@@ -16,10 +16,11 @@ int g_mpi_rank;
 int start_id;
 int end_id;
 int g_max_id = 125000000;
+int count = 0;
 MPI_Offset file_start;
 MPI_Offset file_end;
-std::map<int, std::set<int>> g_adj_list;
-std::map<int, std::set<int>> g_bonus_list;
+std::map<int, std::vector<int>> g_adj_list;
+std::map<int, std::vector<int>> g_bonus_list;
 
 void parse_file_one_rank();
 void parse_file();
@@ -43,10 +44,10 @@ int main(int argc, char** argv) {
     end_id = g_max_id;
   }
 
-  //printf("Rank %d: start_id: %d end_id: %d\n", g_mpi_rank, start_id, end_id);
+  printf("Rank %d: start_id: %d end_id: %d\n", g_mpi_rank, start_id, end_id);
 
   // Print off a hello world message
-  if(g_mpi_rank == 0 || g_mpi_rank==1){
+  if(g_mpi_rank == 0 || g_mpi_rank == 1){
     parse_file();   
   }
   // if(g_mpi_rank==0){
@@ -57,7 +58,7 @@ int main(int argc, char** argv) {
   //if (g_mpi_rank == 0){
    // parse_file();
     //printf("Rank: %d    g_adj_list.size(): %lu\n", g_mpi_rank, g_adj_list.size());
-  }
+  // }
 
 
 
@@ -107,68 +108,20 @@ void parse_file_one_rank(){
 
 
 void parse_file(){
-  char filename[70];
   int lowFile = floor((double)start_id/1000000);
   int highFile = floor((double)end_id/1000000);
   for (int i=0; i<highFile-lowFile+1; i++){
-    sprintf(filename, "/gpfs/u/home/PCP5/PCP5bhgw/scratch/users_%d.txt", lowFile);
-    MPI_File infile;
-    MPI_File_open(MPI_COMM_SELF, filename, MPI_MODE_RDONLY, MPI_INFO_NULL, &infile);
-    MPI_Offset filesize = 0;
-    MPI_File_get_size(infile, &filesize);
-
-    file_start = (g_mpi_rank)*(filesize/g_world_size);
-    if (g_mpi_rank == 0){
-      file_start = 124;
-    }else{
-      file_start -= 50; // allow some overlap to catch newlines
-    }
-    file_end = (g_mpi_rank+1)*(filesize/g_world_size);
-    if (g_mpi_rank == g_world_size-1){
-      file_end = filesize;
-    }
-    // if (g_mpi_rank != 0){
-    // MPI_File_close(&infile);
-    // return;
-    // }
-    MPI_Offset offset = file_start;
-    char * buffer;
-    std::string chunk = "";
-    unsigned int buffer_size = 10000;
-
-    bool skip = (g_mpi_rank != 0);
-
-    while (offset < file_end){
-     // printf("Rank: %d offset: %lld, file_end: %lld, remaining: %lld  size of adj_list: %lu\n", g_mpi_rank, offset, file_end, file_end-offset, g_adj_list.size());
-      buffer = (char *)malloc( (buffer_size + 1)*sizeof(char));
-      MPI_File_read_at(infile, offset, buffer, buffer_size, MPI_CHAR, MPI_STATUS_IGNORE);
-      offset += buffer_size;
-      // end the string
-      buffer[buffer_size] = '\0';
-      std::string new_string(buffer);
-      free(buffer);
-      chunk += new_string;
-
-      if (skip){
-        // skip up to the first newline to remove any broken lines from starting in the middle of the file
-        chunk = chunk.substr(chunk.find('\n')+1);
-      }else{
-        skip = true;
-      }
-
-      int found_nl = chunk.find('\n');
-      while (found_nl < chunk.size()){
-        std::string line = chunk.substr(0, found_nl);
-        add_to_adjlist(line);
-
-        chunk = chunk.substr(found_nl+1);
-        found_nl = chunk.find('\n');
-      }
+    std::stringstream sstm;
+    sstm<<"/gpfs/u/home/PCP5/PCP5bhgw/scratch/users_"<<lowFile<<".txt";
+    std::ifstream infile(sstm.str().c_str());
+    std::string line;
+    while(std::getline(infile,line)){
+      add_to_adjlist(line);  
     }
     //MPI_Barrier(MPI_COMM_WORLD);
-    MPI_File_close(&infile);
     lowFile++;
-    memset(&filename[0], 0, sizeof(filename));
+    sstm.str(std::string());
+    sstm.clear();
   }
   printf("Rank %d: finished read in of own portion of file    %lu\n", g_mpi_rank, g_adj_list.size());
     //sprintf(filename, "com-friendster.ungraph.txt");
@@ -189,7 +142,12 @@ void add_to_adjlist(std::string line){
   int tab_pos = line.find("\t");
   int one = atoi(line.substr(0, tab_pos).c_str());
   int two = atoi(line.substr(tab_pos+1).c_str()); 
+  //std::cout<<start_id<<" "<<end_id<<std::endl;
   if (one >= start_id && one < end_id){
-    g_adj_list[one].insert(two);
+    g_adj_list[one].push_back(two);
+    count++;
+    if(count%10000==0){
+      std::cout<<count<<" "<<one<<std::endl;
+    }
   }
 }
