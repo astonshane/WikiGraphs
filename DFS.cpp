d63b99a3ac25f104a3999dba3826eef8ec2f3cc4
vbhagwani <bhagwani.vatsal@gmail.com>
2016-05-04 15:14:02 -0400

    create boundary edges list

--- DFS.cpp
+++ DFS.cpp
@@ -8,7 +8,7 @@
 #include <vector>
 #include <list>
 
-std::map<int, std::set<int> > g_adj_list;
+std::map<int, std::vector<int> > g_adj_list;
 //sample testing adj_list
 int foo1[] = {2,3,4};
 int foo2[] = {1,3};
@@ -18,29 +18,23 @@ int foo5[] = {3,4};
 int foo6[] = {7};
 int foo7[] = {6};
 
-std::set<int> mySet1(foo1, foo1+3);
-std::set<int> mySet2(foo2, foo2+2);
-std::set<int> mySet3(foo3, foo3+4);
-std::set<int> mySet4(foo4, foo4+3);
-std::set<int> mySet5(foo5, foo5+2);
-std::set<int> mySet6(foo6, foo6+1);
-std::set<int> mySet7(foo7, foo7+1);
+std::vector<int> mySet1(foo1, foo1+3);
+std::vector<int> mySet2(foo2, foo2+2);
+std::vector<int> mySet3(foo3, foo3+4);
+std::vector<int> mySet4(foo4, foo4+3);
+std::vector<int> mySet5(foo5, foo5+2);
+std::vector<int> mySet6(foo6, foo6+1);
+std::vector<int> mySet7(foo7, foo7+1);
 
 std::map<int, bool> visited;
 std::vector<std::vector<int> > connectedComponents;
-
-
-// int vertices = 7;
-// int *visited = new int[vertices];
-// int minRankIndex = 0;
-// int maxRankIndex = 2;
-// int rows = maxRankIndex-minRankIndex+1;
-
+std::map<int, std::vector<int> > boundaryEdges;
 
 void bfs(int u, int ccCounter) {
 	std::list<int> queue;
 	visited[u] = true;
 	queue.push_back(u);
+	int prev = u;
 	while(!queue.empty()) {
 		int s=queue.front();
 		visited[s]=true;
@@ -51,10 +45,22 @@ void bfs(int u, int ccCounter) {
 		}
 		std::cout<<std::endl;
 		queue.pop_front();
-		std::map<int, std::set<int> >::iterator adj_list_itr;
+		std::map<int, std::vector<int> >::iterator adj_list_itr;
 		adj_list_itr = g_adj_list.find(s);
+		if(adj_list_itr == g_adj_list.end()) {
+			std::map<int, std::vector<int> >::iterator it = boundaryEdges.find(prev);
+			if(it != boundaryEdges.end()){
+				it->second.push_back(s);
+			}
+			else{
+				std::vector<int> temp;
+				temp.push_back(s);
+				boundaryEdges[prev] = temp;
+			}
+		}
+		prev = s;
 		if(adj_list_itr != g_adj_list.end()) {
-			for(std::set<int>::iterator itr = adj_list_itr->second.begin(); itr != adj_list_itr->second.end(); itr++) {
+			for(std::vector<int>::iterator itr = adj_list_itr->second.begin(); itr != adj_list_itr->second.end(); itr++) {
 				if(!visited[*itr]){
 					if(find(queue.begin(), queue.end(),*itr)!=queue.end()) {
 						continue;
@@ -78,7 +84,7 @@ int main(int argc, char** argv) {
 	// g_adj_list[6]=mySet6;
 	// g_adj_list[7]=mySet7;
 	int ccCounter = -1;
-	for(std::map<int, std::set<int> >::iterator itr = g_adj_list.begin(); itr!=g_adj_list.end(); itr++) {
+	for(std::map<int, std::vector<int> >::iterator itr = g_adj_list.begin(); itr!=g_adj_list.end(); itr++) {
 		visited[itr->first]=false;
 	}
 	for(std::map<int, bool>::iterator itr =visited.begin(); itr!=visited.end(); itr++) {
